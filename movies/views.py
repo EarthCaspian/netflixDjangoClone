878b1fb2df41c6c662839f6e587a1a47be057886
EarthCaspian <hazar.a@outlook.com>
2023-06-16 21:44:59 +0300

    custom error pages

--- movies/views.py
+++ movies/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 from user.models import *
 # Create your views here.
 
 def index(request):
+    profiller = Profile.objects.filter(user = request.user)
     return render(request, 'index.html')
 
 def movies(request, profilId, slug):
@@ -24,3 +25,8 @@ def video(request, movieId):
     }
     return render(request, 'video.html', context)
 
+def view_404(request, exception):
+    return redirect('/')
+
+def view_500(request):
+    return render(request, 'hata.html')
